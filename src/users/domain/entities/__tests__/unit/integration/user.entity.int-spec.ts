5bc58d0260c9e094c7fdc40cdfa8057d364475f6
Danilo Scipioni <daniloedu@gmail.com>
2024-02-03 11:19:32 -0300

    Integration tests - validate createdAt field on create user

--- src/users/domain/entities/__tests__/unit/integration/user.entity.int-spec.ts
+++ src/users/domain/entities/__tests__/unit/integration/user.entity.int-spec.ts
@@ -54,7 +54,6 @@ describe('UserEntity integration tests', () => {
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
-
     it('Should throw an error when creating a user with invalid password', () => {
       let props: UserProps = {
         ...UserDataBuilder({}),
@@ -77,6 +76,19 @@ describe('UserEntity integration tests', () => {
       props = {
         ...UserDataBuilder({}),
         password: 10 as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+    })
+    it('Should throw an error when creating a user with invalid createdAt', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        createdAt: '2023' as any,
+      }
+      expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        createdAt: 10 as any,
       }
       expect(() => new UserEntity(props)).toThrowError(EntityValidationError)
     })
