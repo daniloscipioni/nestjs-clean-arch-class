ed91025c8a5a420bd270a3dbfe6795db84adaa88
Danilo Scipioni <daniloedu@gmail.com>
2024-02-06 17:36:24 -0300

    Adding SearchableRepositoryInterface

--- src/users/domain/repositories/user.repository.ts
+++ src/users/domain/repositories/user.repository.ts
@@ -1,7 +1,8 @@
-import { RepositoryInterface } from '@/shared/infrastructure/env-config/domain/repositories/repository-contracts'
 import { UserEntity } from '../entities/user.entity'
+import { SearchableRepositoryInterface } from '@/shared/infrastructure/env-config/domain/repositories/searchable-repository-contracts'
 
-export interface UserRepository extends RepositoryInterface<UserEntity> {
+export interface UserRepository
+  extends SearchableRepositoryInterface<UserEntity, any, any> {
   findByEmail(email: string): Promise<UserEntity>
   emailExists(email: string): Promise<void>
 }
