e5fd724e122b1c996ca6c78fddb990e33805eb10
Danilo Scipioni <daniloedu@gmail.com>
2024-02-06 08:39:13 -0300

    Fix imports

--- src/shared/infrastructure/env-config/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
+++ src/shared/infrastructure/env-config/domain/repositories/__tests__/unit/in-memory.repository.spec.ts
@@ -1,7 +1,6 @@
-import { beforeEach, it } from 'node:test'
 import { Entity } from '../../../entities/entity'
-import { InMemoryRepository } from '../../in-memory.repository'
 import { NotFoundError } from '../../../errors/not-found-error'
+import { InMemoryRepository } from '../../in-memory.repository'
 
 type StubEntityProps = {
   name: string
@@ -14,30 +13,25 @@ class StubInMemoryRepository extends InMemoryRepository<StubEntity> {}
 
 describe('InMemoryRepository unit tests', () => {
   let sut: StubInMemoryRepository
-
   beforeEach(() => {
     sut = new StubInMemoryRepository()
   })
-
   it('Should inserts a new entity', async () => {
     const entity = new StubEntity({ name: 'test name', price: 50 })
     await sut.insert(entity)
     expect(entity.toJson()).toStrictEqual(sut.items[0].toJson())
   })
-
   it('Should throw error when entity not found', async () => {
     await expect(sut.findById('fakeId')).rejects.toThrow(
       new NotFoundError('Entity not found'),
     )
   })
-
   it('Should find a entity by id', async () => {
     const entity = new StubEntity({ name: 'test name', price: 50 })
     await sut.insert(entity)
     const result = await sut.findById(entity._id)
     expect(entity.toJson()).toStrictEqual(result.toJson())
   })
-
   it('Should return all entities', async () => {
     const entity = new StubEntity({ name: 'test name', price: 50 })
     await sut.insert(entity)
