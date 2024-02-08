029398a219fca0e106dd9de6a86609c9fc0f43a0
Danilo Scipioni <daniloedu@gmail.com>
2024-02-08 18:19:10 -0300

    Refactoring user repository part 2

--- src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
+++ src/users/infrastructure/database/in-memory/repositories/user-in-memory.repository.ts
@@ -2,13 +2,15 @@ import { ConflictError } from '@/shared/infrastructure/env-config/domain/errors/
 import { NotFoundError } from '@/shared/infrastructure/env-config/domain/errors/not-found-error'
 import { InMemorySearchableRepository } from '@/shared/infrastructure/env-config/domain/repositories/in-memory-searchable.repository'
 import { InMemoryRepository } from '@/shared/infrastructure/env-config/domain/repositories/in-memory.repository'
+import { SortDirection } from '@/shared/infrastructure/env-config/domain/repositories/searchable-repository-contracts'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 
 export class UserInMemoryRepository
   extends InMemorySearchableRepository<UserEntity>
-  implements UserRepository
+  implements UserRepository.Repository
 {
+  sortableFields: string[] = ['name', 'createdAt']
   async findByEmail(email: string): Promise<UserEntity> {
     const entity = this.items.find(item => item.email === email)
     if (!entity) {
@@ -21,5 +23,28 @@ export class UserInMemoryRepository
     if (entity) {
       throw new ConflictError('Email address is already in use')
     }
+  }
+
+  protected async applyFilter(
+    items: UserEntity[],
+    filter: UserRepository.Filter,
+  ): Promise<UserEntity[]> {
+    if (!filter) {
+      return items
+    }
+
+    return items.filter(item => {
+      return item.props.name.toLowerCase().includes(filter.toLowerCase())
+    })
+  }
+
+  protected async applySort(
+    items: UserEntity[],
+    sort: string | null,
+    sortDir: SortDirection | null,
+  ): Promise<UserEntity[]> {
+    return !sort
+      ? super.applySort(items, 'cratedAt', 'desc')
+      : super.applySort(items, sort, sortDir)
   }
 }
