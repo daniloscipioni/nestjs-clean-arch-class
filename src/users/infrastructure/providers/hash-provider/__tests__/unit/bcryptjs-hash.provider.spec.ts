5d8dd422882497336b2a41b319714977ec47dbc6
Danilo Scipioni <daniloedu@gmail.com>
2024-02-12 16:54:58 -0300

    BcryptjsHashProviderunit tests

---
+++ src/users/infrastructure/providers/hash-provider/__tests__/unit/bcryptjs-hash.provider.spec.ts
@@ -0,0 +1,26 @@
+import { BcryptjsHashProvider } from '../../bcryptjs-hash.provider'
+
+describe('BcryptjsHashProvider unit tests', () => {
+  let sut: BcryptjsHashProvider
+  beforeEach(() => {
+    sut = new BcryptjsHashProvider()
+  })
+  it('Should return encrypted password', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+    await expect(hash).toBeDefined()
+  })
+
+  it('Should return false on invalid password and hash comparision', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+    const result = await sut.compareHash('fake', hash)
+    await expect(result).toBeFalsy()
+  })
+  it('Should return false on invalid password and hash comparision', async () => {
+    const password = 'TestPassword123'
+    const hash = await sut.generateHash(password)
+    const result = await sut.compareHash(password, hash)
+    await expect(result).toBeTruthy()
+  })
+})
