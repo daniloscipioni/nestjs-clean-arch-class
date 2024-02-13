3d10d973f23271acfef16c360dc000566ca020aa
Danilo Scipioni <daniloedu@gmail.com>
2024-02-13 11:52:57 -0300

    Adding ListUsersUseCase - part 2

---
+++ src/shared/application/dto/pagination-output.ts
@@ -0,0 +1,25 @@
+import { Entity } from '@/shared/infrastructure/env-config/domain/entities/entity'
+import { SearchResult } from '@/shared/infrastructure/env-config/domain/repositories/searchable-repository-contracts'
+
+export type PaginationOutput<Item = any> = {
+  items: Item[]
+  total: number
+  currentPage: number
+  lastPage: number
+  perPage: number
+}
+
+export class PaginationOutputMapper {
+  static toOutput<Item = any>(
+    items: Item[],
+    result: SearchResult<Entity>,
+  ): PaginationOutput<Item> {
+    return {
+      items,
+      total: result.total,
+      currentPage: result.currentPage,
+      lastPage: result.lastPage,
+      perPage: result.perPage,
+    }
+  }
+}
--- src/users/application/usecases/listusers.usecase.ts
+++ src/users/application/usecases/listusers.usecase.ts
@@ -1,10 +1,15 @@
 import { UserRepository } from '@/users/domain/repositories/user.repository'
 import { UseCase as DefaultUseCase } from '@/shared/application/usecases/use-case'
 import { SearchInput } from '@/shared/application/dto/search-input'
+import {
+  PaginationOutput,
+  PaginationOutputMapper,
+} from '@/shared/application/dto/pagination-output'
+import { UserOutput, UserOutputMapper } from '../dto/user-output'
 
 namespace ListUsersUseCase {
   export type Input = SearchInput
-  export type Output = void
+  export type Output = PaginationOutput<UserOutput>
 
   export class UseCase implements DefaultUseCase<Input, Output> {
     // Injeção de dependencia
@@ -13,7 +18,15 @@ namespace ListUsersUseCase {
     async execute(input: Input): Promise<Output> {
       const params = new UserRepository.SearchParams(input)
       const searchResult = await this.userRepository.search(params)
-      return
+      return this.toOutput(searchResult)
+    }
+
+    private toOutput(searchResult: UserRepository.SearchResult): Output {
+      const items = searchResult.items.map(item => {
+        return UserOutputMapper.toOutput(item)
+      })
+
+      return PaginationOutputMapper.toOutput(items, searchResult)
     }
   }
 }
