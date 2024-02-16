7faf304a7325569d2877e231954c7f2ea1adfb56
Danilo Scipioni - CI&T <daniloedu@gmail.com>
2024-02-16 12:34:25 -0300

    Adding DeleteUserUseCase unit test

--- src/users/application/usecases/__tests__/unit/delete-user.usecase.spec.ts
+++ src/users/application/usecases/__tests__/unit/delete-user.usecase.spec.ts
@@ -1,17 +1,16 @@
 import { UserInMemoryRepository } from '@/users/infrastructure/database/in-memory/repositories/user-in-memory.repository'
-import GetUserUseCase from '../../getuser.usecase'
 import { NotFoundError } from '@/shared/infrastructure/env-config/domain/errors/not-found-error'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '@/users/domain/entities/testing/helpers/user-data-builder'
+import DeleteUserUseCase from '../../delete-user.usecase copy'
 
-describe('GetUserUseCase unit tests', () => {
-  let sut: GetUserUseCase.UseCase
+describe('DeleteUserUseCase unit tests', () => {
+  let sut: DeleteUserUseCase.UseCase
   let repository: UserInMemoryRepository
 
   beforeEach(() => {
     repository = new UserInMemoryRepository()
-
-    sut = new GetUserUseCase.UseCase(repository)
+    sut = new DeleteUserUseCase.UseCase(repository)
   })
   it('Should throws error when entity not found', async () => {
     await expect(() => sut.execute({ id: 'fakeId' })).rejects.toThrow(
@@ -19,19 +18,13 @@ describe('GetUserUseCase unit tests', () => {
     )
   })
 
-  it('Should be able to get user profile', async () => {
-    const spyFindById = jest.spyOn(repository, 'findById')
+  it('Should be able to delete user profile', async () => {
+    const spyDelete = jest.spyOn(repository, 'delete')
     const items = [new UserEntity(UserDataBuilder({}))]
     repository.items = items
-
+    expect(repository.items).toHaveLength(1)
     const result = await sut.execute({ id: items[0]._id })
-    expect(spyFindById).toHaveBeenCalledTimes(1)
-    expect(result).toMatchObject({
-      id: items[0].id,
-      name: items[0].name,
-      email: items[0].email,
-      password: items[0].password,
-      createdAt: items[0].createdAt,
-    })
+    expect(spyDelete).toHaveBeenCalledTimes(1)
+    expect(repository.items).toHaveLength(0)
   })
 })
