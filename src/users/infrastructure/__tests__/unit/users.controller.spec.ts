1e449d82c25bf2c21f3a9a766910337e674cc4b0
Danilo Scipioni <daniloedu@gmail.com>
2024-02-18 15:53:57 -0300

    UsersController unit tests - update method

--- src/users/infrastructure/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/__tests__/unit/users.controller.spec.ts
@@ -4,6 +4,8 @@ import { SignupUseCase } from '@/users/application/usecases/signup.usecase'
 import { SignupDto } from '../../dto/signup.dto'
 import { SigninUseCase } from '@/users/application/usecases/signin.usecase'
 import { SigninDto } from '../../dto/signin.dto'
+import UpdateUserUseCase from '@/users/application/usecases/update-user.usecase'
+import { UpdateUserDto } from '../../dto/update-user.dto'
 
 describe('UsersController unit tests', () => {
   let sut: UsersController
@@ -54,5 +56,19 @@ describe('UsersController unit tests', () => {
     const result = await sut.login(input)
     expect(output).toMatchObject(result)
     expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input)
+  })
+
+  it('should update user', async () => {
+    const output: UpdateUserUseCase.Output = props
+    const mockUpdateUserUseCase = {
+      execute: jest.fn().mockReturnValue(Promise.resolve(output)),
+    }
+    sut['updateUserUseCase'] = mockUpdateUserUseCase as any
+    const input: UpdateUserDto = {
+      name: 'new name',
+    }
+    const result = await sut.update(id, input)
+    expect(output).toMatchObject(result)
+    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({ id, ...input })
   })
 })
