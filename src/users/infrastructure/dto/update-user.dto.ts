869d5921c26ff076dce751e9752a10c1b504e5d2
Danilo Scipioni <daniloedu@gmail.com>
2024-03-16 10:01:11 -0300

    Config ValidationPipe and DTOs

--- src/users/infrastructure/dto/update-user.dto.ts
+++ src/users/infrastructure/dto/update-user.dto.ts
@@ -1,6 +1,9 @@
 import UpdateUserUseCase from '@/users/application/usecases/update-user.usecase'
+import { IsNotEmpty, IsString } from 'class-validator'
 
 // Omite o campo id pois ele vem como parametro e não no body
 export class UpdateUserDto implements Omit<UpdateUserUseCase.Input, 'id'> {
+  @IsString()
+  @IsNotEmpty()
   name: string
 }
