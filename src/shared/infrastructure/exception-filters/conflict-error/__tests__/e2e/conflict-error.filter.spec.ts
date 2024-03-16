901ba354c095951038bd80d1d058eda6abd24332
Danilo Scipioni <daniloedu@gmail.com>
2024-03-16 10:50:15 -0300

    Adding ConflictErrorFilter e2e tests

---
+++ src/shared/infrastructure/exception-filters/conflict-error/__tests__/e2e/conflict-error.filter.spec.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, INestApplication } from '@nestjs/common'
+import { ConflictErrorFilter } from '../../conflict-error.filter'
+import { Test, TestingModule } from '@nestjs/testing'
+import request from 'supertest'
+import { ConflictError } from '@/shared/infrastructure/env-config/domain/errors/conflict-error'
+
+@Controller('stub')
+class StubController {
+  @Get()
+  index() {
+    throw new ConflictError('Conflicting data')
+  }
+}
+
+describe('ConflictErrorFilter (e2e)', () => {
+  let app: INestApplication
+  let module: TestingModule
+
+  beforeAll(async () => {
+    module = await Test.createTestingModule({
+      controllers: [StubController],
+    }).compile()
+    app = module.createNestApplication()
+    app.useGlobalFilters(new ConflictErrorFilter())
+    await app.init()
+  })
+
+  afterAll(async () => {
+    await module.close()
+  })
+
+  it('should catch a ConflictError', () => {
+    return request(app.getHttpServer()).get('/url').expect(409).expect({
+      status: 409,
+      error: 'Conflict',
+      message: 'Email address already used',
+    })
+  })
+})
