6a22555317cd286f6304026908cfe1d544bcc273
Danilo Scipioni <daniloedu@gmail.com>
2024-03-17 10:45:49 -0300

    UsersController e2e tests - findOne method

--- src/users/infrastructure/__tests__/e2e/findone.e2e-spec.ts
+++ src/users/infrastructure/__tests__/e2e/findone.e2e-spec.ts
@@ -12,13 +12,11 @@ import { instanceToPlain } from 'class-transformer'
 import { applyGlobalConfig } from '@/global-config'
 import { UserEntity } from '@/users/domain/entities/user.entity'
 import { UserDataBuilder } from '@/users/domain/entities/testing/helpers/user-data-builder'
-import { UpdateUserDto } from '../../dto/update-user.dto'
 
 describe('UsersController e2e tests', () => {
   let app: INestApplication
   let module: TestingModule
   let repository: UserRepository.Repository
-  let updateUserDto: UpdateUserDto
   const prismaService = new PrismaClient()
   let entity: UserEntity
 
@@ -38,48 +36,26 @@ describe('UsersController e2e tests', () => {
   })
 
   beforeEach(async () => {
-    updateUserDto = {
-      name: 'test name',
-    }
     await prismaService.user.deleteMany()
     entity = new UserEntity(UserDataBuilder({}))
     await repository.insert(entity)
   })
 
-  describe('PUT /users/:id', () => {
-    it('should update a user', async () => {
-      updateUserDto.name = 'test name'
+  describe('GET /users/:id', () => {
+    it('should get a user', async () => {
       const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send(updateUserDto)
+        .get(`/users/${entity._id}`)
         .expect(200)
 
-      const user = await repository.findById(entity._id)
-
-      const presenter = UsersController.userToResponse(user.toJson())
+      const presenter = UsersController.userToResponse(entity.toJson())
       const serialized = instanceToPlain(presenter)
 
       expect(res.body.data).toStrictEqual(serialized)
     })
 
-    it('should return error with 422 code when the request body is invalid', async () => {
-      const res = await request(app.getHttpServer())
-        .put(`/users/${entity._id}`)
-        .send({})
-        .expect(422)
-
-      expect(res.body.error).toBe('Unprocessable Entity')
-      // Para comparar array e objetos tem que ser o toEqual
-      expect(res.body.message).toEqual([
-        'name should not be empty',
-        'name must be a string',
-      ])
-    })
-
     it('should return error with 404 code when throw NotFoundError with invalid id', async () => {
       const res = await request(app.getHttpServer())
-        .put(`/users/fakeId`)
-        .send(updateUserDto)
+        .get(`/users/fakeId`)
         .expect(404)
         .expect({
           status: 404,
