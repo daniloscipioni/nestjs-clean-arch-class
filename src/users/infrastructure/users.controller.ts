6ae5fc45291cde00d40a8dc2572b6ddfdeefa702
Danilo Scipioni <daniloedu@gmail.com>
2024-03-25 11:18:39 -0300

    Adding AuthGuard

--- src/users/infrastructure/users.controller.ts
+++ src/users/infrastructure/users.controller.ts
@@ -10,6 +10,7 @@ import {
   HttpCode,
   Query,
   Put,
+  UseGuards,
 } from '@nestjs/common'
 
 import { SignupDto } from './dto/signup.dto'
@@ -30,6 +31,7 @@ import {
   UserPresenter,
 } from './presenters/user.presenter'
 import { AuthService } from '@/auth/infrastructure/auth.service'
+import { AuthGuard } from '@/auth/infrastructure/auth.guard'
 
 @Controller('users')
 export class UsersController {
@@ -71,18 +73,21 @@ export class UsersController {
     return this.authService.generateJwt(output.id)
   }
 
+  @UseGuards(AuthGuard)
   @Get()
   async search(@Query() searchParams: ListUsersDto) {
     const output = await this.listUsersUseCase.execute(searchParams)
     return UsersController.listUsersToResponse(output)
   }
 
+  @UseGuards(AuthGuard)
   @Get(':id')
   async findOne(@Param('id') id: string) {
     const output = await this.getUserUseCase.execute({ id }) // o sinal "+" na frente do parametro transforma o param em número
     return UsersController.userToResponse(output)
   }
 
+  @UseGuards(AuthGuard)
   @Put(':id')
   async update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const output = await this.updateUserUseCase.execute({
@@ -92,6 +97,7 @@ export class UsersController {
     return UsersController.userToResponse(output)
   }
 
+  @UseGuards(AuthGuard)
   @Patch(':id')
   async updatePassword(
     @Param('id') id: string,
@@ -104,6 +110,7 @@ export class UsersController {
     return UsersController.userToResponse(output)
   }
 
+  @UseGuards(AuthGuard)
   @HttpCode(204)
   @Delete(':id')
   async remove(@Param('id') id: string) {
